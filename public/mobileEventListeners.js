d655ebd45175abde5341abc44d93db75f0ac5d75
Synclair Wang <synclairwang11@yahoo.com>
2016-11-03 16:50:48 -0400

    fixing mobile taps

--- public/mobileEventListeners.js
+++ public/mobileEventListeners.js
@@ -1,65 +1,65 @@
 $(() => {
   console.log('mobileEventListeners.js LINKED')
 
-  $('img').tap((e) => {
+  $('img').on('tap', ((e) => {
     const modalID = $(e.target).data('modalid');
     console.log('MODAL ID IS: '+ modalID)
     $(modalID).css('display', 'block');
     $('.cover-page').css('display', 'block')
-  })
+  }))
 
-  $('.cover-page').tap(() => {
+  $('.cover-page').on('tap', (() => {
     $('.modal').hide()
     $('.cover-page').css('display', 'none')
-  })
+  }))
 
-  $('.header-container').tap(() => {
+  $('.header-container').on('tap', (() => {
     $('.modal').hide()
     $('.cover-page').css('display', 'none')
-  })
+  }))
 
-  $('.fa-times').tap(() => {
+  $('.fa-times').on('tap', (() => {
     $('.modal').hide()
     $('.cover-page').css('display', 'none')
-  })
+  }))
 
-  $('.main-header').tap(() => {
+  $('.main-header').on('tap', (() => {
     $('.sub-header').css('display', 'inline')
-  })
+  }))
 
-  $('.main-header').tap(() => {
+  $('.main-header').on('tap', (() => {
     $('.sub-header').css('display', 'none')
-  })
+  }))
 
 
-  $('.accounts-button').tap(() => {
+  $('.accounts-button').on('tap', (() => {
     $('.account-modal').css('display', 'block')
     $('.page-container-cover').css('display', 'block')
     $('.dropdown-content').hide()
-  })
+  }))
 
-  $('.account-close-window').tap(() => {
+  $('.account-close-window').on('tap', (() => {
     $('.account-modal').css('display', 'none')
     $('.page-container-cover').css('display', 'none')
-  })
+  }))
 
-  $('.fa-user').tap(() => {
+  $('.fa-user').on('tap', (() => {
     $('.dropdown-content').show()
-  })
+  }))
 
-  $('.tsundoku-it-broken').tap(() => {
+  $('.tsundoku-it-broken').on('tap', (() => {
     $('.account-modal').css('display', 'block')
     $('.page-container-cover').css('display', 'block')
-  })
+  }))
 
-  $('.tsundoku-it').tap(() => {
+  $('.tsundoku-it').on('tap', (() => {
     $('.tsundokud-modal').css('display', 'block')
     $('.modal').css('display','none')
     setTimeout(() => {
       $('.tsundokud-modal').css('display', 'none')
       $('.cover-page').css('display', 'none')
     }, 1000)
-  })
+  }))
 
 
 
