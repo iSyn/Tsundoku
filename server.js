9e5be0f3f64d6daf75324f54ac4cc6ef197fac27
Synclair Wang <synclairwang11@yahoo.com>
2016-11-20 21:59:02 -0500

    footer added back in

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || process.argv[2] || 3000
 
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
